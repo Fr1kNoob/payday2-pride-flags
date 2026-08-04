af0c7beb56da8af923b7cd830f3e079cb627be16
Fr1kNoob <Fr1kNoob@users.noreply.github.com>
2026-08-04 19:22:05 +0300

    Add localization to release

--- lua/PrideFlags.lua
+++ lua/PrideFlags.lua
@@ -1,7 +1,7 @@
 _G.PrideFlags = _G.PrideFlags or {}
 
 PrideFlags.ModPath = PrideFlags.ModPath or ModPath
-PrideFlags.Version = "1.1.5"
+PrideFlags.Version = "1.1.6"
 PrideFlags.MessageId = "PrideFlags:hello"
 PrideFlags.SaveFile = SavePath .. "PrideFlags.json"
 PrideFlags.ValidFlags = {
