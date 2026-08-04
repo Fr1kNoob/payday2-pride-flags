442fe2b6622fe02d2770e49def714baf21cfcf69
Fr1kNoob <Fr1kNoob@users.noreply.github.com>
2026-08-04 19:02:22 +0300

    Fix archive paths for SuperBLT verifier

--- lua/PrideFlags.lua
+++ lua/PrideFlags.lua
@@ -1,7 +1,7 @@
 _G.PrideFlags = _G.PrideFlags or {}
 
 PrideFlags.ModPath = PrideFlags.ModPath or ModPath
-PrideFlags.Version = "1.1.0"
+PrideFlags.Version = "1.1.1"
 PrideFlags.MessageId = "PrideFlags:hello"
 PrideFlags.SaveFile = SavePath .. "PrideFlags.json"
 PrideFlags.ValidFlags = {
